10e7dcb75023af167586efe1edf4c820a74754b7
高红 <honggao@tianler.com>
2016-03-01 10:43:18 +0800

    ejs

--- controllers/reg_controller.js
+++ controllers/reg_controller.js
@@ -10,13 +10,11 @@ module.exports = {
 
     reg: function(req, res){
 
-       /* res.rander('../view/register.html',{
-            user:'',
-            email:'',
-            password:'',
-            sex:''
-        });*/
-        res.render('register',{});
+        res.render('register',{
+            user:'张三',
+            email:'1233@qq.com',
+            password:'111111',
+            sex:'0'});
     }
 
 };
